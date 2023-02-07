9a297ce70f6f10ca302daf6f423e7d62692333de
Tim Leach <timleach635@gmail.com>
2023-02-07 13:54:31 +0000

    Add "honks" page

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import * as path from "path";
+import { format } from "date-fns";
 
 // express
 import express from "express";
@@ -7,8 +8,34 @@ app.use(express.static(path.join(__dirname, "/static")));
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "/views"));
 
+// routes
 app.get("/", (req, res) => {
   res.render("welcome", { currentUser: { isAuthenticated: false } });
+});
+
+app.get("/honks", (req, res) => {
+  const filter = req.query["filter"];
+  const search = req.query["search"];
+  const page = req.query["page"];
+
+  res.render("honks", {
+    currentUser: { isAuthenticated: false },
+    honks: {
+      total: 1,
+      items: [
+        {
+          user: { id: 1, username: "tim" },
+          content: "I am a honk!",
+          timestamp: new Date(),
+        }
+      ],
+      iterPages: () => [1],
+      page,
+    },
+    format,
+    filter,
+    search,
+  })
 });
 
 app.listen(3000);
