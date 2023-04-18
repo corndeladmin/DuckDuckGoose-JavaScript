ece5484252c88041e25a89cacf9cd804bb74dccd
TedWhi <ted.while@softwire.com>
2023-04-18 13:57:04 +0100

    Add "users" page
    Remove follow functionality
    
    
    fix

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import * as path from "path";
+import { format } from "date-fns";
 
 // express
 import express from "express";
 // database
+import { QueryTypes } from "sequelize";
 import { db, User as DbUser } from "./database/index";
+import { iterPages } from "./pagination";
 // sessions
 import session from "express-session";
 import sessionSequelize from "connect-session-sequelize";
@@ -18,6 +21,7 @@ app.use(express.urlencoded());
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "/views"));
 
+const itemsPerPage = 5;
 declare global {
   namespace Express {
     interface User extends DbUser {}
@@ -84,6 +88,100 @@ app.get("/", (req, res) => {
       isAuthenticated: req.user !== undefined,
       id: req.user?.id,
     },
+  });
+});
+
+app.get("/users", async (req, res) => {
+  const filter = req.query["filter"];
+  const search = req.query["search"];
+  const page: number = req.query["page"] ? parseInt(req.query["page"] as string) : 1;
+  
+  const whereClauses: string[] = [];
+  
+  if (search) {
+    whereClauses.push(`u."username" ILIKE CONCAT('%', :search, '%')`);
+  }
+
+  // have to use raw SQL because sequelize isn't very well written
+  // notice, no string interpolation of variables, to avoid SQL injection
+  // https://xkcd.com/327/
+  const query = `
+    SELECT
+      u."id" AS "id",
+      u."username" AS "username",
+      u."nHonks" AS "nHonks"
+    FROM (
+      SELECT
+        u."id" AS "id",
+        u."username" AS "username",
+        u."nHonks" AS "nHonks"
+      FROM (
+        SELECT
+          u."id" AS "id",
+          u."username" AS "username",
+          COUNT(h."id") AS "nHonks"
+        FROM
+          users u
+          LEFT JOIN honks h
+            ON u."id" = h."userId"
+        ${search ? `WHERE u."username" ILIKE CONCAT('%', :search, '%')` : ""}
+        GROUP BY u."id"
+        ORDER BY u."username"
+      ) AS u
+      GROUP BY u."id", u."username", u."nHonks"
+    ) AS u
+    GROUP BY u."id", u."username", u."nHonks"
+    LIMIT :limit OFFSET :offset;
+  `;
+  const countQuery = `
+    SELECT
+      COUNT(*) AS "total"
+    FROM users u
+    ${whereClauses.length > 0 ? `WHERE ${whereClauses.join(" AND ")}` : ""};
+  `;
+  
+  const users = await db.query<{
+    id: number,
+    username: string,
+    nHonks: string,
+  }>(
+    query,
+    {
+      replacements: {
+        limit: itemsPerPage,
+        offset: (page - 1) * itemsPerPage,
+        userId: req.user?.id,
+        search: search,
+      },
+      type: QueryTypes.SELECT,
+    }
+  );
+  const userTotalResult = await db.query<{ total: string }>(
+    countQuery,
+    {
+      replacements: {
+        userId: req.user?.id,
+        search: search,
+      },
+      type: QueryTypes.SELECT,
+    }
+  );
+  const userTotal: number = parseInt(userTotalResult[0].total);
+
+  res.render("users", {
+    currentUser: {
+      isAuthenticated: req.user !== undefined,
+      id: req.user?.id,
+    },
+    users: {
+      total: userTotal,
+      items: users,
+      pages: iterPages(page, Math.ceil(userTotal / itemsPerPage)),
+      page: page,
+    },
+    format,
+    filter,
+    search,
   });
 });
 
